fc33868e91c5e0cca3199191100a4cd8b38a534c
mtm <Stafr95@mail.ru>
2026-03-04 16:03:12 +0500

    breadcrumb connection

--- news-smi/index.php
+++ news-smi/index.php
@@ -1,17 +1,21 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 
-$APPLICATION->SetTitle("Завод Амега в ".$SEO_CITY." | Новости и события");
-$APPLICATION->SetPageProperty("description", "Амега - завод пластиковых окон в ".$SEO_CITY.". Новости и события от производителя пластиковых окон, дверей, натяжных потолков, балконных блоков");
+$APPLICATION->SetTitle("Завод Амега в " . $SEO_CITY . " | Новости и события");
+$APPLICATION->SetPageProperty("description", "Амега - завод пластиковых окон в " . $SEO_CITY . ". Новости и события от производителя пластиковых окон, дверей, натяжных потолков, балконных блоков");
 $APPLICATION->SetPageProperty("keywords", "");
-$APPLICATION->SetPageProperty("title", "Завод Амега в ".$SEO_CITY." | Новости и события");
+$APPLICATION->SetPageProperty("title", "Завод Амега в " . $SEO_CITY . " | Новости и события");
 
 Bitrix\Main\Page\Asset::getInstance()->addJs("/news-smi/js/script.js");
 
 $breadcrumbs = [
     array(
-        "title"=> "Главная",
-        "url"=> "/",
+        "title" => "Главная",
+        "url" => "/",
+    ),
+    array(
+        "title" => "Новости",
+        "url" => "",
     ),
 ]
 
