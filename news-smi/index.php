9d1d3eb6059e4a87cac7551e1562d295e9271384
mtm <Stafr95@mail.ru>
2026-03-11 12:50:52 +0500

    require file

--- news-smi/index.php
+++ news-smi/index.php
@@ -1,21 +1,21 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
-$APPLICATION->SetTitle("Завод Амега в " . $SEO_CITY . " | Новости и события");
-$APPLICATION->SetPageProperty("description", "Амега - завод пластиковых окон в " . $SEO_CITY . ". Новости и события от производителя пластиковых окон, дверей, натяжных потолков, балконных блоков");
+$APPLICATION->SetTitle("Завод Амега в ".$SEO_CITY." | Новости и события");
+$APPLICATION->SetPageProperty("description", "Амега - завод пластиковых окон в ".$SEO_CITY.". Новости и события от производителя пластиковых окон, дверей, натяжных потолков, балконных блоков");
 $APPLICATION->SetPageProperty("keywords", "");
-$APPLICATION->SetPageProperty("title", "Завод Амега в " . $SEO_CITY . " | Новости и события");
+$APPLICATION->SetPageProperty("title", "Завод Амега в ".$SEO_CITY." | Новости и события");
 
 Bitrix\Main\Page\Asset::getInstance()->addJs("/news-smi/js/script.js");
 
 $breadcrumbs = [
     array(
-        "title" => "Главная",
-        "url" => "/",
+        "title"=> "Главная",
+        "url"=> "/",
     ),
     array(
-        "title" => "Новости",
-        "url" => "",
+        "title"=> "Новости",
+        "url"=> "",
     ),
 ];
 
@@ -23,8 +23,10 @@ require($_SERVER["DOCUMENT_ROOT"] . "/parts/breadcrumbs.php");
 require("hero.php");
 require("search.php");
 require("news.php");
+require("social.php");
 ?>
 
-<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media.css">
 
 <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
