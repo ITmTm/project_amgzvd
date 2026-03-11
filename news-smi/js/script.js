e67bbb1d35dda9d4990d4cb33dbe08ce91db39b4
mtm <Stafr95@mail.ru>
2026-03-11 12:45:23 +0500

    show/hide cross

---
+++ news-smi/js/script.js
@@ -0,0 +1,68 @@
+// Пагинация
+$(document).on('click', '.pagination-item', function(e) {
+  e.preventDefault();
+  const url = $(this).attr('href');
+  if (!url || $(this).hasClass('disabled') || $(this).hasClass('dots')) return;
+
+  $('html, body').animate({
+    scrollTop: $('.news-wrapper').offset().top - 200
+  }, 500);
+
+  loadNews(url);
+});
+
+// Показать/скрыть крестик
+$(document).on('input', '#news-search-form input[name="q"]', function(e) {
+  const $clear = $(this).siblings('.input-clear');
+  if ($(this).val().length > 0) {
+    $clear.addClass('visible');
+  } else {
+    $clear.removeClass('visible');
+  }
+});
+
+// Поиск
+$(document).on('submit', '#news-search-form', function(e) {
+  e.preventDefault();
+  const query = $(this).find('input[name="q"]').val();
+  const url = '/news-smi/?' + (query ? 'q=' + encodeURIComponent(query) : '');
+
+  window.history.pushState({}, '', url);
+  loadNews(url, query);
+});
+
+// Сброс
+$(document).on('click', '.btn-brand-red-clean', function(e) {
+  e.preventDefault();
+  const url = $(this).attr('href');
+  $('input[name="q"]').val('');
+  window.history.pushState({}, '', url);
+  loadNews(url);
+});
+
+// Общая функция загрузки
+function loadNews(url, query) {
+  $('#news-loader').removeClass('d-none');
+
+  $.ajax({
+    url: url,
+    type: 'GET',
+    success: function(response) {
+      const $response = $(response);
+      let newContent = $response.find('.news-content').html();
+      if (!newContent) {
+        newContent = $response.filter('.news-content').html();
+      }
+      if (newContent) {
+        $('.news-content').html(newContent);
+        $('#news-loader').addClass('d-none');
+      }
+      if (query) {
+        $('input[name="q"]').val(query);
+      }
+    },
+    error: function() {
+      $('#news-loader').addClass('d-none');
+    }
+  });
+}
