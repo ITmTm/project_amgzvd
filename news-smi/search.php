8320d9d771a5b8da1a3f526668677568f42a6ad8
mtm <Stafr95@mail.ru>
2026-03-12 22:40:58 +0500

    change files

---
+++ news-smi/search.php
@@ -0,0 +1,20 @@
+<section class="pt-5">
+    <div class="new-container">
+        <h2 class="title-h3 mb-5">Поиск нужных новостей</h2>
+        <form class="w-100" id="news-search-form">
+            <div class="news-search row align-items-center w-100 g-0">
+                <div class="col mb-md-0 mb-3">
+                    <div class="input-search w-100">
+                        <input placeholder="Поиск" type="search" class="w-100" name="q"
+                               value="<?= $_GET["q"] ?>"
+                               aria-label="Поиск по новостям">
+                    </div>
+                </div>
+                <div class="col-md-auto col-12 text-end">
+                    <button type="submit" class="btn btn-brand-red">Найти</button>
+                    <a href="/news-smi/" class="btn btn-brand-red-clean">Сбросить</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
