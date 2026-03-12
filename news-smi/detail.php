dd77ab95c59ffe5a71c266e25edfd7d4aa0b172e
mtm <Stafr95@mail.ru>
2026-03-12 22:40:51 +0500

    change files

---
+++ news-smi/detail.php
@@ -0,0 +1,37 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+$APPLICATION->SetTitle("");
+$APPLICATION->SetPageProperty("description", "Амега - завод пластиковых окон в ".$SEO_CITY.". У нас вы можете купить качественные пластиковые окна по доступным ценам");
+$APPLICATION->SetPageProperty("keywords", "");
+$APPLICATION->SetPageProperty("title", "Завод пластиковых окон в ".$SEO_CITY.", купить окна от производителя");
+function getXMLElement($CODE){
+    $FILTER_MATERIAL = Array('GLOBAL_ACTIVE'=>'Y', 'CODE'=>$CODE);
+    $SELECT_MATERIAL = Array("ID");
+    $db_list = CIBlockElement::GetList(array(), $FILTER_MATERIAL, false, false, $SELECT_MATERIAL);
+    $response = array();
+    while($ar_result = $db_list->GetNext())
+    {
+        $response[] = $ar_result;
+    }
+    return $response[0];
+}
+$res = getXMLElement($_REQUEST["CODE"]);
+
+$APPLICATION->IncludeComponent(
+    "bitrix:news.detail",
+    "news-smi", // шаблон
+    Array(
+        "IBLOCK_ID" => "48",  // ID информационного блока
+        "IBLOCK_TYPE" => "news",  // тип информационного блока
+        "ELEMENT_ID" => $res["ID"],  // параметр передаваемой страницы
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        "ADD_SECTIONS_CHAIN" => "N",
+        "SET_BROWSER_TITLE" => "Y",
+        "SET_META_DESCRIPTION" => "Y",
+        "SET_TITLE" => "Y",
+        "ADD_ELEMENT_CHAIN" => "Y"
+    ),
+    false
+);
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
