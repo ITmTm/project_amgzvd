2cdfd8104948aa3cf60f657230b51dbd421cc618
mtm <Stafr95@mail.ru>
2026-03-12 22:41:06 +0500

    change files

---
+++ news-smi/social.php
@@ -0,0 +1,26 @@
+<section class="news-social">
+    <div class="new-container">
+        <h2 class="primary-section-title title-h3">Амега с соцсетях</h2>
+
+        <div class="social-body">
+            <div class="social-text">
+                Нажимайте, а затем подписывайтесь на наши социальные сети и следите, чтобы быть в курсе наших новостей и событий из жизни завода
+            </div>
+
+            <div class="social-links">
+                <a href="https://vkvideo.ru/@amegazavod1/?roistat_visit=2523465" class="subtitle-h3" target="_blank">
+                    <img src="/local/templates/amegazavod_new/img/vk-new.svg" alt="VK Video">
+                    VK видео
+                </a>
+                <a href="https://vk.com/amegazavod1/?roistat_visit=2523465" class="subtitle-h3" target="_blank">
+                    <img src="/local/templates/amegazavod_new/img/vk-group.svg" alt="VK">
+                    Вконтакте
+                </a>
+                <a href="https://t.me/amega_zavod/?roistat_visit=2523465" class="subtitle-h3" target="_blank">
+                    <img src="/local/templates/amegazavod_new/img/tg-new.svg" alt="Tg">
+                    Телеграм
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
