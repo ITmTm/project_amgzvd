55f5e093621aab2d62bca5cdf2295cb2b777a5a0
mtm <Stafr95@mail.ru>
2026-03-12 22:40:53 +0500

    change files

---
+++ news-smi/hero.php
@@ -0,0 +1,16 @@
+<section class="hero-section banner">
+    <div class="new-container">
+        <div class="banner__text">
+            <h1 class="title-h2 banner__title">
+                Новости нашего <br>
+                завода
+            </h1>
+            <div class="text-m banner__description">
+                Актуальные новости о развитии завода, производстве, новинках, событиях и мероприятиях, в которых
+                участвует завод. Мы публикуем только важные анонсы, объявления и новости, чтобы вы всегда были в курсе
+                событий жизни завода «Амега»
+            </div>
+            <img src="pic/banner-mini.avif" alt="Новости нашего завода" class="banner__image">
+        </div>
+    </div>
+</section>
