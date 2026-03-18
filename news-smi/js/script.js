3a57814d9cb3f27bf6c207f3825a0137f98735c2
mtm <Stafr95@mail.ru>
2026-03-18 19:09:30 +0500

    create pagination

--- news-smi/js/script.js
+++ news-smi/js/script.js
@@ -1,1 +1,10 @@
 // Пагинация
+$(document).on('click', '.pagination-item', function(e) {
+  e.preventDefault();
+  const url = $(this).attr('href');
+  if (!url || $(this).hasClass('disabled') || $(this).hasClass('dots')) return;
+
+
+
+  loadNews(url);
+});
