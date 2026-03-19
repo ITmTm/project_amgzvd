33ea49ec5ad8ec9fd19fe90c3196f01cb763f831
mtm <Stafr95@mail.ru>
2026-03-19 18:08:00 +0500

    creation section detail

--- news-smi/detail.php
+++ news-smi/detail.php
@@ -2,6 +2,7 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("");
+$APPLICATION->SetPageProperty("description", "Амега - завод пластиковых окон в ".
 
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
