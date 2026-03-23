c7e138125c2a5ed6870e5874d10231d71c9dd2b4
mtm <Stafr95@mail.ru>
2026-03-23 17:39:18 +0500

    creation function

--- news-smi/detail.php
+++ news-smi/detail.php
@@ -4,6 +4,12 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("");
 $APPLICATION->SetPageProperty("description", "Амега - завод пластиковых окон в " . $SEO_CITY . ". У нас вы можете купить качественные пластиковые окна по доступным ценам");
 $APPLICATION->SetPageProperty("keywords", "");
-$APPLICATION->SetPageProperty("title", "Завод пластиковых окон в ".$SEO_CITY.", купить окна от производителя")
+$APPLICATION->SetPageProperty("title", "Завод пластиковых окон в " . $SEO_CITY . ", купить окна от производителя")
+
+    function getXMLElement($CODE){
+
+    }
+
+
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
