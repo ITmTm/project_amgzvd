f4ca5485ccbd23009ec7fddfdef0ab6f8a121cb0
mtm <Stafr95@mail.ru>
2026-03-23 17:38:39 +0500

    creation section detail

--- news-smi/detail.php
+++ news-smi/detail.php
@@ -4,5 +4,6 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("");
 $APPLICATION->SetPageProperty("description", "Амега - завод пластиковых окон в " . $SEO_CITY . ". У нас вы можете купить качественные пластиковые окна по доступным ценам");
 $APPLICATION->SetPageProperty("keywords", "");
+$APPLICATION->SetPageProperty("title", "Завод пластиковых окон в ".$SEO_CITY.", купить окна от производителя")
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
