a1247f1d09699cb31b821f5d4152920bfc20863e
mtm <Stafr95@mail.ru>
2026-03-25 17:50:52 +0500

    creation section social

--- news-smi/social.php
+++ news-smi/social.php
@@ -3,7 +3,11 @@
         <h2 class="primary-section-title title-h3">Амега с соцсетях</h2>
 
         <div class="social-body">
-           
+            <div class="social-text">
+                Нажимайте, а затем подписывайтесь на наши социальные сети и следите, чтобы быть в курсе наших новостей и событий из жизни завода
+            </div>
+
+   
         </div>
     </div>
 </section>
