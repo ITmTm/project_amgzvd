d3747e63d32fe0859648163518159d2f2633dd4a
mtm <Stafr95@mail.ru>
2026-03-25 17:50:39 +0500

    creation section social

--- news-smi/social.php
+++ news-smi/social.php
@@ -2,6 +2,8 @@
     <div class="new-container">
         <h2 class="primary-section-title title-h3">Амега с соцсетях</h2>
 
-      
+        <div class="social-body">
+           
+        </div>
     </div>
 </section>
