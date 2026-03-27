2a8c0ab39cd9759bde7ffa37e7e3c6fe939a3e19
mtm <Stafr95@mail.ru>
2026-03-27 21:22:00 +0500

    creation section search

--- news-smi/search.php
+++ news-smi/search.php
@@ -3,7 +3,10 @@
         <h2 class="title-h3 mb-5">Поиск нужных новостей</h2>
         <form class="w-100" id="news-search-form">
             <div class="news-search row align-items-center w-100 g-0">
- 
+                <div class="col mb-md-0 mb-3">
+          
+                </div>
+
             </div>
         </form>
     </div>
