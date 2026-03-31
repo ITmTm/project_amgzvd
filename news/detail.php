10beeaaa6b8ef018476c8dea12728a82185cc5a9
mtm <Stafr95@mail.ru>
2026-03-31 19:27:14 +0500

    creation section for news

--- news/detail.php
+++ news/detail.php
@@ -3,3 +3,4 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("");
 $APPLICATION->SetPageProperty("description", "Амега - завод пластиковых окон в ".$SEO_CITY.". У нас вы можете купить качественные пластиковые окна по доступным ценам");
+$APPLICATION->SetPageProperty("keywords", "");
