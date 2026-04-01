857b9cad5a5f8aec7334cffb631a64e2ac41cff3
mtm <Stafr95@mail.ru>
2026-04-01 18:35:05 +0500

    creation section for news

--- news/index.php
+++ news/index.php
@@ -3,3 +3,4 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("");
 $APPLICATION->SetPageProperty("description", " Новости и события завода Амега в ".$SEO_CITY.". Производитель пластиковых ПВХ окон, дверей, алюминевых конструкций.");
+$APPLICATION->SetPageProperty("keywords", "");
