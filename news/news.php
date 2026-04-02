112381df50e48bcea42856d3849dcabd31b7ab32
mtm <Stafr95@mail.ru>
2026-04-02 22:04:34 +0500

    creation section for news

--- news/news.php
+++ news/news.php
@@ -10,7 +10,11 @@
         if (isset($_GET["page"])) {
             $page = $_GET["page"];
         }
+        $arrFilter = array(
+            array("NAME" => "%" . $query . "%")
+        );
 
+                            </div>
                         </div>
                     </div>
                 </div>
