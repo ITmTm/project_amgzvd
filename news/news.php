eaa92d599b00972e4099b060ca86e4fa9e738fbf
mtm <Stafr95@mail.ru>
2026-04-02 22:03:47 +0500

    creation section for news

--- news/news.php
+++ news/news.php
@@ -1,5 +1,8 @@
 <section class="py-5">
     <div class="container">
- 
+        <?
+        global $arrFilter;
+
+        <? require_once($_SERVER["DOCUMENT_ROOT"] . "/parts/pagination.php");?>
     </div>
 </section>
