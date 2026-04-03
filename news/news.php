18abd2c5b4e4798a898bef7179c3433a8cb6b7d4
mtm <Stafr95@mail.ru>
2026-04-03 18:35:59 +0500

    creation section for news

--- news/news.php
+++ news/news.php
@@ -31,7 +31,9 @@
                     <div class="row">
                         <div class="col-4">
                             <a href="<?= $newsItem["DETAIL_PAGE_URL"] ?>" class="mb-3">
-         
+                                <img src="<?= CFile::GetPath($newsItem["PREVIEW_PICTURE"]) ?>"
+
+                                    Читать польностью
                                 </a>
                             </div>
                         </div>
