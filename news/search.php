8084326a9d0c3c16d654a6cab795bd6eac2a24f6
mtm <Stafr95@mail.ru>
2026-04-05 15:33:16 +0500

    creation section for news

--- news/search.php
+++ news/search.php
@@ -7,7 +7,9 @@
                         <label for="news-search" class="title-search">Новости</label>
                     </div>
                     <div class="col mb-md-0 mb-3">
-
+                        <div class="input-search w-100">
+  
+                    </div>
                     <div class="col-md-auto col-12">
                         <button type="submit" class="btn btn-brand-red">Найти</button>
                         <a href="/news/" class="btn btn-brand-red-clean">Сбросить</a>
