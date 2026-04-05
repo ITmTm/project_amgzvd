33dd3d845ede922cd89196d3a78dc217d3dbc276
mtm <Stafr95@mail.ru>
2026-04-05 15:32:47 +0500

    creation section for news

--- news/search.php
+++ news/search.php
@@ -4,7 +4,9 @@
             <div class="row w-100">
                 <div class="row w-100">
                     <div class="col-md-auto col-12 mb-md-0 mb-3">
+                        <label for="news-search" class="title-search">Новости</label>
 
+                        <a href="/news/" class="btn btn-brand-red-clean">Сбросить</a>
                     </div>
                 </div>
             </div>
