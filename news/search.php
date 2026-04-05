7f5c4980227a5399bb4722e628e9a695a7b94ed8
mtm <Stafr95@mail.ru>
2026-04-05 15:32:22 +0500

    creation section for news

--- news/search.php
+++ news/search.php
@@ -1,7 +1,9 @@
 <section class="pt-5">
     <div class="container">
         <form class="d-flex align-items-center w-100">
-  
+            <div class="row w-100">
+      
+            </div>
         </form>
     </div>
 </section>
