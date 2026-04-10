fa1125de3a4dca115cab9e12453b5adfcbd42dfa
mtm <Stafr95@mail.ru>
2026-04-10 22:06:35 +0500

    creation section

--- about/banner_top.php
+++ about/banner_top.php
@@ -1,6 +1,8 @@
 <section class="banner_top">
   <img src="/about/img/banner.png" alt="баннер">
   <div class="banner_top_content text-white">
+    <div class="container">
 
+    $videoSrc = '/assets/video/about-company.mp4';
     require($_SERVER["DOCUMENT_ROOT"] . "/parts/modals/aboutModalVideo.php");?>
 </section>
