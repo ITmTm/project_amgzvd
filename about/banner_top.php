24a45dcd35167b9387d7d3b5c45f7505315be393
mtm <Stafr95@mail.ru>
2026-04-10 22:06:45 +0500

    creation section

--- about/banner_top.php
+++ about/banner_top.php
@@ -2,7 +2,9 @@
   <img src="/about/img/banner.png" alt="баннер">
   <div class="banner_top_content text-white">
     <div class="container">
+      <h1 class="banner_top_title" style="color:white">О КОМПАНИИ</h1>
 
+    <?php
     $videoSrc = '/assets/video/about-company.mp4';
     require($_SERVER["DOCUMENT_ROOT"] . "/parts/modals/aboutModalVideo.php");?>
 </section>
