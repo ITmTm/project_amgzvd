468a2777b755424245c23f91141282ddfaf48cd5
mtm <Stafr95@mail.ru>
2026-04-10 22:04:35 +0500

    writing a script

--- about/js/script.js
+++ about/js/script.js
@@ -2,5 +2,7 @@ $(document).ready(()=> {
     document.querySelector('.sign_tour').addEventListener('click', ()=>{
         openModalMail('Записаться на экскурсию')
     })
-
+    document.querySelector('.send_partner').addEventListener('click', ()=>{
+        openModalMail('Стать партнером')
+    })
 })
