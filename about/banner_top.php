af741d49d5a3a5271f6d4936a3e3f9e4221b06fa
mtm <Stafr95@mail.ru>
2026-04-10 22:07:21 +0500

    creation section

--- about/banner_top.php
+++ about/banner_top.php
@@ -3,7 +3,9 @@
   <div class="banner_top_content text-white">
     <div class="container">
       <h1 class="banner_top_title" style="color:white">О КОМПАНИИ</h1>
+      <p class="banner_top_description">Компания «Амега» - это завод по производству светопрозрачных конструкций из ПВХ и алюминия в России. Завод реализуют продукцию на дилерском рынке и рынке корпоративной застройки в 8 регионах</p>
 
+  </div>
     <?php
     $videoSrc = '/assets/video/about-company.mp4';
     require($_SERVER["DOCUMENT_ROOT"] . "/parts/modals/aboutModalVideo.php");?>
