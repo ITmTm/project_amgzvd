eb1be49a7767ca060a649fea57fdbbb4089c5613
mtm <Stafr95@mail.ru>
2026-04-14 23:51:32 +0500

    creation section

--- about/description.php
+++ about/description.php
@@ -5,7 +5,22 @@
         <h2 class="fs-3 fw-bold mb-4">Наши продукты</h2>
         <div class="mb-4">Завод «Амега» - это мультибрендовое предприятие. Мы производим окна, балконы, двери, порталы из 6 профильных ПВХ систем, 3 алюминиевых систем, 3 видов фурнитуры для ПВХ, 5 видов фурнитуры для алюминия и 8 видов стекол. Ассортимент завода «Амега» может удовлетворить любой запрос потребителя от эконом до премиума.</div>
         <div>
+          <a href="/assets/files/presentation-amega.pdf" target="_blank" class="btn btn-outline-danger btn-outline-brand-red">Ассортиментная матрица <img src="/imgs/icons/download-black.svg" alt=""></a>
+        </div>
+      </div>
 
+          </div>
+          <div class="d-flex col-auto">
+            <a href="https://redirect.appmetrica.yandex.com/serve/964480543599132047" class="me-2"><img src="/imgs/app_store.png" alt=""></a>
+            <a href="https://redirect.appmetrica.yandex.com/serve/964480543599132047"><img src="/imgs/google_play.png" alt=""></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row g-3 align-items-center mb-4">
+      <div class="col-lg col-12 order-lg-1 order-2 d-flex flex-column">
+        <h2 class="fs-3 fw-bold mb-4">Партнерство</h2>
+        <div class="mb-4">Компания «Амега» зарекомендовала себя как проверенного партнера для многих российских и зарубежных поставщиков. Мы сотрудничаем с Profine, Exprof, Proplex, РЕХАУ, Maco, Roto, ФУТУРУСС, Pilkington, Салаватстекло, AGC Group, УСПК, Татпроф.</div>
         <div>
           <div class="btn btn-danger btn-brand-red send_partner">Стать партнером <img src="/imgs/icons/arrows-right.svg"></div>
         </div>
