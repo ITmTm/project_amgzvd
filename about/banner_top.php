d0ed581ea52a383556eee3abb61affdcf0e0aa7c
mtm <Stafr95@mail.ru>
2026-04-14 23:47:10 +0500

    creation section

--- about/banner_top.php
+++ about/banner_top.php
@@ -5,7 +5,9 @@
       <h1 class="banner_top_title" style="color:white">О КОМПАНИИ</h1>
       <p class="banner_top_description">Компания «Амега» - это завод по производству светопрозрачных конструкций из ПВХ и алюминия в России. Завод реализуют продукцию на дилерском рынке и рынке корпоративной застройки в 8 регионах</p>
       <div class="d-flex btn_banner mb-sm-4 mb-0">
+        <div class="btn btn-brand-red btn-danger px-4 d-flex align-items-center open-video">
 
+        </div>
       </div>
     </div>
   </div>
