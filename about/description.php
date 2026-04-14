71a0e79a2772f1ac77df147faaafe1627c4107e2
mtm <Stafr95@mail.ru>
2026-04-14 23:49:36 +0500

    creation section

--- about/description.php
+++ about/description.php
@@ -1,6 +1,8 @@
 <section class="py-5">
   <div class="container">
     <div class="row g-3 align-items-center mb-4">
+      <div class="col-lg col-12 order-lg-1 order-2 d-flex flex-column">
 
+    </div>
   </div>
 </section>
