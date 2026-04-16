cadcc87a0710ff88637847b3dc0784d05a8f3bff
mtm <Stafr95@mail.ru>
2026-04-16 21:47:47 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -2,4 +2,5 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("Амега в ".$SEO_CITY." - компания по производству металлических и пвх конструкций");
+$APPLICATION->SetPageProperty("title", "Амега в ".$SEO_CITY." - компания по производству металлических и пвх конструкций");
 
