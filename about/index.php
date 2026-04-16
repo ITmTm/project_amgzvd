6cd3f0eaf2146e613baf0e7e7f74d4174ff2b473
mtm <Stafr95@mail.ru>
2026-04-16 21:47:57 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -4,4 +4,4 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Амега в ".$SEO_CITY." - компания по производству металлических и пвх конструкций");
 $APPLICATION->SetPageProperty("title", "Амега в ".$SEO_CITY." - компания по производству металлических и пвх конструкций");
 $APPLICATION->SetPageProperty("description", "Компания Амега в ".$SEO_CITY." ★ 13 лет на рынке ★ 34 секунды - время изготовление окна ★ 2 550 конструкций в сутки ★ Выгодные условия для дилеров ★ Система скидок ★ Собственная сеть логистики");
-
+$APPLICATION->SetPageProperty("keywords", "");
