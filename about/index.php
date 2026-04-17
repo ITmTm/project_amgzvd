ceeb3a0df70e1c7ea01abbeee9666359efabf6a8
mtm <Stafr95@mail.ru>
2026-04-17 20:18:05 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -33,3 +33,4 @@ require($_SERVER["DOCUMENT_ROOT"] . "/parts/statistics/statistics.php");?>
             </div>
             <div class="col-lg-4 col-12 order-lg-2 order-1">
                 <img src="/about/img/1.png" alt="" class="w-100">
+            </div>
