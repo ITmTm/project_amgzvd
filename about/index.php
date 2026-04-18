3a4ff4d84d99b6a276573d8d3f16c58e0a52b313
mtm <Stafr95@mail.ru>
2026-04-18 21:59:16 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -45,4 +45,6 @@ require($_SERVER["DOCUMENT_ROOT"] . "/parts/statistics/statistics.php");?>
             </div>
             <div>
               <div class="btn btn-outline-danger btn-outline-brand-red sign_tour">
-                Записаться на экскурсию на завод <img src="/imgs/icons/arrows-right-black.svg" alt=""></div>
+                Записаться на экскурсию на завод <img src="/imgs/icons/arrows-right-black.svg" alt="">
+              </div>
+            </div>
