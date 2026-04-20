c612b2224ccb084bd5494a4d015c835baae386de
mtm <Stafr95@mail.ru>
2026-04-20 21:14:05 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -96,3 +96,4 @@ require($_SERVER["DOCUMENT_ROOT"] . "/parts/statistics/statistics.php"); ?>
             Записаться на самовывоз
             <img src="/imgs/icons/arrows-right-black.svg" alt="→">
           </a>
+            <? elseif ((int)$CITY['ID'] === 244764) : ?>
