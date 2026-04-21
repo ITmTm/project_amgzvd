4e38b1e9da3a78adc7515b855c5f9e85da19900c
mtm <Stafr95@mail.ru>
2026-04-21 21:22:49 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -114,3 +114,5 @@ require($_SERVER["DOCUMENT_ROOT"] . "/parts/statistics/statistics.php"); ?>
       </div>
       <div class="col-lg col-12 d-flex flex-column">
         <h2 class="fs-3 fw-bold mb-4">Технологии</h2>
+        <div class="mb-4">В 2020г. компания «Амега» запустила передовой IT-сервис Framer. Framer – личный кабинет дилера завода «Амега», который позволяет посчитать, оплатить, передать в производство заказ и регистрировать рекламации полностью онлайн.</div>
+        
