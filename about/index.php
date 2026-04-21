d1284287528f0f5f500d7f904424e5cf8c71d231
mtm <Stafr95@mail.ru>
2026-04-21 21:23:14 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -117,3 +117,4 @@ require($_SERVER["DOCUMENT_ROOT"] . "/parts/statistics/statistics.php"); ?>
         <div class="mb-4">В 2020г. компания «Амега» запустила передовой IT-сервис Framer. Framer – личный кабинет дилера завода «Амега», который позволяет посчитать, оплатить, передать в производство заказ и регистрировать рекламации полностью онлайн.</div>
         <div class="d-flex align-items-center row row-cols-md-2 row-cols-1 g-3">
           <div class="col-auto">
+            <a target="_blank" href="https://framer.ru/?  utm_source=amegazavod&utm_medium=about&utm_campaign=technologies" class="btn btn-outline-brand-blue me-2">Перейти в сервис Framer <img src="/imgs/icons/arrows-right-black.svg" alt=""></a>
