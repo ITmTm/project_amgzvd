df13113032a7f06503827c3106d62e77854bc958
mtm <Stafr95@mail.ru>
2026-04-23 19:33:48 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -145,3 +145,4 @@ require($_SERVER["DOCUMENT_ROOT"] . "/parts/statistics/statistics.php"); ?>
 </section>
 <section class="mb-5 pb-5">
     <? require($_SERVER["DOCUMENT_ROOT"] . "/parts/forms/dealer.php");?>
+</section>
