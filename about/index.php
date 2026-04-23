8ef52f70399beb07ec65afed977420f1bef221a2
mtm <Stafr95@mail.ru>
2026-04-23 19:34:38 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -137,7 +137,9 @@ require($_SERVER["DOCUMENT_ROOT"] . "/parts/statistics/statistics.php"); ?>
       <div class="col-lg-4 col-12 order-lg-2 order-1">
         <img src="/about/img/2.png" alt="" class="w-100">
       </div>
-
+    </div>
+  </div>
+</section>
   <section class="map_home_section">
       <? require($_SERVER["DOCUMENT_ROOT"] . "/parts/map/map.php");?>
   </section>
