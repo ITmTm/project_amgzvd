17bdcaf8dfbafdded460f982e2b61330d4c9bc13
mtm <Stafr95@mail.ru>
2026-04-23 19:34:28 +0500

    creation section

--- about/index.php
+++ about/index.php
@@ -140,4 +140,10 @@ require($_SERVER["DOCUMENT_ROOT"] . "/parts/statistics/statistics.php"); ?>
     </div>
   </div>
 </section>
-
+  <section class="map_home_section">
+      <? require($_SERVER["DOCUMENT_ROOT"] . "/parts/map/map.php");?>
+  </section>
+  <section class="mb-5 pb-5">
+      <? require($_SERVER["DOCUMENT_ROOT"] . "/parts/forms/dealer.php");?>
+  </section>
+<? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
